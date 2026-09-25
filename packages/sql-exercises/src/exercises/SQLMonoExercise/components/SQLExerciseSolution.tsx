f86cdb0ddd3eaf788c5af104a74f655e52fd3053
Hildo Bijl <hildobijl@gmail.com>
2026-09-25 19:15:02 +0200

    Minor update to default SQLExerciseSolution component

--- packages/sql-exercises/src/exercises/SQLMonoExercise/components/SQLExerciseSolution.tsx
+++ packages/sql-exercises/src/exercises/SQLMonoExercise/components/SQLExerciseSolution.tsx
@@ -1,8 +1,14 @@
+import { Stack, Typography } from '@mui/material'
+
 import { SQLDisplay } from '@sqlvalley/sql'
 import { useSolution } from '@sqlvalley/input-exercise-components'
 
 export function SQLExerciseSolution() {
 	const solution = useSolution()
 	if (!solution || typeof solution.query !== 'string') throw new Error('A SQL solution must contain a query string.')
-	return <SQLDisplay>{solution.query}</SQLDisplay>
+	return <Stack spacing={2}>
+		<Typography component="p">One way of solving the above problem is through the following query.</Typography>
+		<SQLDisplay>{solution.query}</SQLDisplay>
+		<Typography component="p">As always, there are multiple ways to set this up, so other solutions may be fine too, as long as they give the same output.</Typography>
+	</Stack>
 }
